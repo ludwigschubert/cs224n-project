f13cb46145f38fed6ebef9fdd1c30bb57939491e
leonidk <lkeselman@gmail.com>
2017-03-11 16:46:34 -0800

    who knows

--- data/sentiment-treebank/fetch.sh
+++ data/sentiment-treebank/fetch.sh
@@ -1,1 +1,8 @@
-wget http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+#rm stanfordSentimentTreebank.zip
+#wget http://nlp.stanford.edu/~socherr/stanfordSentimentTreebank.zip
+#unzip stanfordSentimentTreebank.zip
+#rm -rf __MACOSX
+rm trainDevTestTrees_PTB.zip
+wget http://nlp.stanford.edu/sentiment/trainDevTestTrees_PTB.zip
+unzip trainDevTestTrees_PTB.zip
+sudo pip install pytreebank
