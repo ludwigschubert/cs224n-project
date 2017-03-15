1b6f703598359e13530bd4090d6bea4cc8828198
leonidk <lkeselman@gmail.com>
2017-03-14 23:21:06 -0700

    updated sentiment script

--- data/sentiment-treebank/convert.py
+++ data/sentiment-treebank/convert.py
@@ -2,6 +2,16 @@ import pandas as pd
 
 import pytreebank
 import json
+import json
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def tokenize_body(text):
+    document = text.replace('-\n', '').replace('- \n', ' ').replace('\n', ' ').replace('\t', ' ')
+    sentences = sent_tokenize(document)
+    result =  '<d> <p> ' + ' '.join(['<s> ' + ' '.join(word_tokenize(sentence)).lower() + ' </s>' for sentence in sentences]) + ' </p> </d>'
+    return result
+
+
 #dataset = pytreebank.load_sst()
 input_files = ['trees/train.txt','trees/dev.txt','trees/test.txt']
 dataset = {key.split('/')[-1].split('.')[0]: pytreebank.import_tree_corpus(key) for key in input_files}
@@ -10,7 +20,7 @@ dataset = {key: [(sorted([(len(s),s) for s in t1.to_lines()])[::-1][0][1],t1.lab
 
 dout = []
 for key in dataset:
-	dout += [{'data':x[0],'label':[x[1]],'set':key} for x in dataset[key]]
+	dout += [{'data':tokenize_body(x[0].lower()),'label':[x[1]],'set':key} for x in dataset[key]]
 with open('data.json','w') as fp:
 	json.dump(dout, fp,sort_keys=True,indent=4, separators=(',', ': '))
 #sentences = pd.read_csv('stanfordSentimentTreebank/datasetSentences.txt',sep='\t')
