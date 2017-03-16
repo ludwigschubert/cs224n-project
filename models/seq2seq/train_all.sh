cf2b2aa2ad57e25fc903b37d6708adff4f32bacd
Ludwig Schubert <git@ludwigschubert.de>
2017-03-15 22:22:32 -0700

    Who needs makefiles anyway

---
+++ models/seq2seq/train_all.sh
@@ -0,0 +1,89 @@
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=duc2004 \
+  --cnn=0 \
+  --train_embedding=0 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=nips-abstract-title \
+  --cnn=0 \
+  --train_embedding=0 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=squad \
+  --cnn=0 \
+  --train_embedding=0 \
+  --output_root="." \
+
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=duc2004 \
+  --cnn=1 \
+  --train_embedding=0 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=nips-abstract-title \
+  --cnn=1 \
+  --train_embedding=0 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=squad \
+  --cnn=1 \
+  --train_embedding=0 \
+  --output_root="." \
+
+
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=duc2004 \
+  --cnn=1 \
+  --train_embedding=1 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=nips-abstract-title \
+  --cnn=1 \
+  --train_embedding=1 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=squad \
+  --cnn=1 \
+  --train_embedding=1 \
+  --output_root="." \
+
+
+
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=duc2004 \
+  --cnn=0 \
+  --train_embedding=1 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=nips-abstract-title \
+  --cnn=0 \
+  --train_embedding=1 \
+  --output_root="." \
+
+python decoderTT.py \
+  --runmode=train \
+  --dataset_name=squad \
+  --cnn=0 \
+  --train_embedding=1 \
+  --output_root="." \
